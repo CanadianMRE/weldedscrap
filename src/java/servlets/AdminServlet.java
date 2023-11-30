00ff0f4cd1213ad1f31f765839b6daf55abf735e
WeldedScrap <WeldedScrap@DESKTOP-KKRF9KE.localdomain>
2023-11-30 10:01:05 -0800

    Merge origin/master
    Conflicts:
    	web/WEB-INF/cart.jsp

--- src/java/servlets/AdminServlet.java
+++ src/java/servlets/AdminServlet.java
@@ -7,7 +7,6 @@ package servlets;
 
 import java.io.IOException;
 import java.util.List;
-import java.math.BigDecimal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -16,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import models.Users;
-import services.StripeAccess;
 import services.UserService;
 
 /**
